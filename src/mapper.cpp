0072bd88a47b63300900864edd429f7cc7af26e2
Logan Smyth <loganfsmyth@gmail.com>
2011-07-24 23:39:21 -0400

    Convert mapper write to a switch statement as well

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -58,7 +58,7 @@ uint8_t Mapper::read(uint16_t addr) {
 
     // SRAM: [0x6000 - 0x8000)
     case 0x6000:
-      return sram[addr & 0x3FFF];
+      return sram[addr & 0x1FFF];
 
     // Bank 1: [0x8000 - 0xC000)
     case 0x8000:
@@ -74,64 +74,69 @@ uint8_t Mapper::read(uint16_t addr) {
 
 void Mapper::write(uint16_t addr, uint8_t value) {
 
-  // RAM
-  if (addr >= 0x0000 && addr < 0x2000) {
-    ram[addr & 0x7FF] = value;
-  }
+  switch (addr&0xE000) {
 
-  // GPU Register access
-  else if (addr >= 0x2000 && addr < 0x4000) {
-    gpu->write(addr & 0x7, value);
-  }
+    // RAM: [0x0000 - 0x2000)
+    case 0x0000:
+      ram[addr&0x7FF] = value;
+      break;
 
-  // Non-GPU Register access
-  else if (addr >= 0x4000 && addr < 0x4020) {
+    // GPU: [0x2000 - 0x4000)
+    case 0x2000:
+      gpu->write(addr & 0x7, value);
+      break;
 
-    // DMA from PRG-RAM to SPR-RAM
-    if (addr == 0x4014) {
-      uint8_t* src;
-      value *= 0x100;
+    // Expansion + IO: [0x4000 - 0x6000)
+    case 0x4000:
+      // DMA from PRG-RAM to SPR-RAM
+      if (addr == 0x4014) {
+        uint8_t* src;
+        value *= 0x100;
 
-      // From RAM
-      if (addr < 0x20) {
-        src = &ram[value];
+        // From RAM
+        if (addr < 0x20) {
+          src = &ram[value];
+        }
+
+        // From SRAM
+        else if (addr < 0x80) {
+          src = &sram[value & 0x1F00];
+        }
+
+        else if (addr < 0xC0) {
+          src = &bank1[value & 0x3F00];
+        }
+
+        else {
+          src = &bank2[value & 0x3F00];
+        }
+
+        memcpy(gpu->sprite_ram, src, 0x100);
       }
+      else if (addr < 0x4020) {
 
-      // From SRAM
-      else if (addr < 0x80) {
-        src = &sram[value & 0x1F00];
       }
-
-      else if (addr < 0xC0) {
-        src = &bank1[value & 0x3F00];
-      }
-
       else {
-        src = &bank2[value & 0x3F00];
+
       }
+      break;
 
-      memcpy(gpu->sprite_ram, src, 0x100);
-    }
-  }
+    // SRAM: [0x6000 - 0x8000)
+    case 0x6000:
+      sram[addr & 0x1FFF] = value;
+      break;
 
-  // Expansion ROM
-  else if (addr >= 0x4020 && addr < 0x6000) {
+    // Bank 1: [0x8000 - 0xC000)
+    case 0x8000:
+    case 0xA000:
+      bank1[addr & 0x3FFF] = value;
+      break;
 
-  }
-
-  // SRAM
-  else if (addr >= 0x6000 && addr < 0x8000) {
-    sram[addr & 0x1FFF] = value;
-  }
-
-  // PRG-ROM Bank 1
-  else if (addr >= 0x8000 && addr < 0xC000) {
-    bank1[addr & 0x3FFF] = value;
-  }
-
-  // PRG-ROM Bank 2
-  else if (addr >= 0xC000 && addr < 0x10000) {
-    bank2[addr & 0x3FFF] = value;
+    // Bank 2: [0xC000 - 0x10000)
+    case 0xC000:
+    case 0xE000:
+      bank2[addr & 0x3FFF] = value;
+      break;
   }
 
 }
